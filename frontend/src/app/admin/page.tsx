1a14f6f559c3c4239ba63a8624d00fe46e6ddb14
athaliasanjaya <athalia.sanjaya@gmail.com>
2026-07-03 17:18:25 +0800

    alia: tambah halaman di src, components, folder constant

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -2,57 +2,35 @@
 
 import Link from 'next/link';
 import { useAuthStore } from '@/store/auth.store';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+
 
 export default function AdminPage() {
-
-  const router = useRouter();
-  const { user, hydrated, logout } = useAuthStore();
+  const { user } = useAuthStore();
   
-useEffect(() => {
-    if (!hydrated) return;
-
-  if (!user) {
-    router.replace('/login');
-    return;
-  }
-
-  if (user.role !== 'ADMIN') {
-    router.replace('/');
-  }
-}, [user, hydrated, router]);
-
-if (!hydrated) return <p>Loading...</p>;
-
-if (!user || user.role !== 'ADMIN') return null;
-
-
-
 const menu = [
   {
-    name:'Kurir',
-    path:'/admin/kurir'
+    name:'Pesanan Baru',
+    path:'/admin/pesanan-baru'
   },
   {
-    name:'Layanan',
-    path:'/admin/layanan'
+    name:'Pengantaran',
+    path:'/admin/pengantaran'
   },
   {
-    name:'Outlet',
-    path:'/admin/outlet'
+    name:'Kelola Pesanan',
+    path:'/admin/kelola-pesanan'
   },
   {
-    name:'Pesanan',
-    path:'/admin/pesanan'
+    name:'Pembayaran',
+    path:'/admin/pembayaran'
   },
   {
-    name:'Slot Operasional',
-    path:'/admin/slot-operasional'
+    name:'Riwayat Pesanan',
+    path:'/admin/riwayat-pesanan'
   },
   {
-    name:'Transaksi',
-    path:'/admin/transaksi'
+    name:'Profil',
+    path:'/admin/profil'
   }
 ];
 
@@ -64,21 +42,10 @@ const menu = [
       </h1>
 
       <p className="mt-2 text-gray-600">
-        Halo, {user.nama}
+        Halo, {user?.nama}
       </p>
 
-    <button
-        onClick={() => {
-            logout();
-            router.replace('/login');
-        }}
-        className="mt-4 px-4 py-2 bg-red-500 text-white rounded-xl text-sm font-semibold hover:bg-red-600 transition"
-        >
-        Keluar
-    </button>
-
       <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-8">
-
         {menu.map(item => (
 
           <Link
@@ -106,11 +73,7 @@ const menu = [
           </Link>
 
         ))}
-
-
       </div>
-
-
     </div>
   );
 }
